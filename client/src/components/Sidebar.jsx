30e4605f8f0a6f4eff4985a3d03e4a96336a6421
sparrow7559 <virajgupta2604@gmail.com>
2025-09-18 23:59:02 +0530

    Implement Sidebar component with user navigation and search functionality

--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -1,9 +1,47 @@
 import React from "react";
+import assets from "../assets/assets";
+import { useNavigate } from "react-router-dom";
 
-const Sidebar = () => {
+const Sidebar = ({ selectedUser, setSelectedUser }) => {
+  const navigate = useNavigate();
+
   return (
-    <div>
-      <h1>Sidebar</h1>
+    <div
+      className={`bg-[#8185B2]/10 h-full p-5 rounded-r-xl overflow-y-scroll text-white ${
+        selectedUser ? "max-md:hidden" : ""
+      }`}
+    >
+      <div className="pb-5 ">
+        <div className="flex justify-between items-center">
+          <img src={assets.logo} alt="logo" className="max-w-40" />
+          <div className="relative py-2 group">
+            <img
+              src={assets.menu_icon}
+              alt="menu_icon"
+              className="max-h-5 cursor-pointer"
+            />
+            <div className="absolute top-full right-0 z-20 w-32 p-5 rounded-md bg-[#282142] border border-gray-600 text-gray-100 hidden group-hover:block">
+              <p
+                onClick={() => navigate("/profile")}
+                className="cursor-pointer text-sm"
+              >
+                Edit Profile
+              </p>
+              <hr className="my-2 border-t border-gray-500" />
+              <p className="cursor-pointer text-sm">Logout</p>
+            </div>
+          </div>
+        </div>
+
+        <div className="bg-[#282142] rounded-full flex items-center hap-2 py-2 px-4 mt-5">
+          <img src={assets.search_icon} alt="search" className="w-3" />
+          <input
+            type="text"
+            className="bg-transparent border-none outline-none text-white text-xs placeholder-[#c8c8c8] flex-1"
+            placeholder="Search User..."
+          />
+        </div>
+      </div>
     </div>
   );
 };
