d631d4f6a9008cc268e741b47869c182dc65ecda
sparrow7559 <virajgupta2604@gmail.com>
2025-09-21 08:46:01 +0530

    Implement RightSidebar component to display selected user details

--- client/src/components/RightSidebar.jsx
+++ client/src/components/RightSidebar.jsx
@@ -1,10 +1,28 @@
 import React from "react";
+import assets from "../assets/assets";
 
-const RightSidebar = () => {
+const RightSidebar = (selectedUser, setSelectedUser) => {
   return (
-    <div>
-      <h1>RightSidebar</h1>
-    </div>
+    selectedUser && (
+      <div
+        className={`bg-[#8185B2]/10 text-white w-full relative overflow-y-scroll ${
+          selectedUser ? "max-md:hidden" : ""
+        }`}
+      >
+        <div className="pt-16 flex flex-col items-center gap-2 text-xs font-light mx-auto">
+          <img
+            src={selectedUser?.profilePic || assets.avatar_icon}
+            alt=""
+            className="w-20 aspect-[1/1] rounded-full"
+          />
+          <h1 className="px-10 text-xl font-medium mx-auto flex items-center gap-2">
+            <p className="w-2 h-2 rounded-full bg-green-500"></p>
+            {selectedUser.fullName}
+          </h1>
+          <p className="px-10 mx-auto">{selectedUser.bio}</p>
+        </div>
+      </div>
+    )
   );
 };
 
