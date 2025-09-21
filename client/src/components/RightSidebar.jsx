7c08703279a570f013ba83f64146483ff55c7dcc
sparrow7559 <virajgupta2604@gmail.com>
2025-09-21 10:10:43 +0530

    Added logout button on the right sidebar

--- client/src/components/RightSidebar.jsx
+++ client/src/components/RightSidebar.jsx
@@ -36,6 +36,10 @@ const RightSidebar = ({ selectedUser }) => {
             ))}
           </div>
         </div>
+
+        <button className="absolute bottom-5 left-1/2 transform -translate-x-1/2 bg-gradient-to-r from-purple-400 to-violet-600 text-white border-none text-sm font-light py-2 px-20 rounded-full cursor-pointer">
+          Logout
+        </button>
       </div>
     )
   );
