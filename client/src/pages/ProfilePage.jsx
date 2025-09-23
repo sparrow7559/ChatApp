312918da49f7871d25ae99b8895379f14b16c764
sparrow7559 <virajgupta2604@gmail.com>
2025-09-23 18:25:35 +0530

    Add form handling and input fields to ProfilePage for user details

--- client/src/pages/ProfilePage.jsx
+++ client/src/pages/ProfilePage.jsx
@@ -8,10 +8,18 @@ const ProfilePage = () => {
   const [name, setName] = useState("Martin Johnson");
   const [bio, setBio] = useState("Hi everyone, I am using QuickChat");
 
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    navigate("/");
+  };
+
   return (
     <div className="min-h-screen bg-cover bg-no-repeat flex items-center justify-center">
       <div className="w-5/6 max-w-2xl backdrop-blur-2xl text-gray-300 border-2 border-gray-600 flex items-center justify-between max-sm:flex-col-reverse rounded-lg">
-        <form action="" className="flex flex-col gap-5 p-10 flex-1">
+        <form
+          onSubmit={handleSubmit}
+          className="flex flex-col gap-5 p-10 flex-1"
+        >
           <h3 className="text-lg">Profile Details</h3>
           <label
             htmlFor="avatar"
@@ -34,10 +42,36 @@ const ProfilePage = () => {
               alt=""
               className={`w-12 h-12 ${selectedImage && "rounded-full"}`}
             />
-            upload profile image
+            Upload Profile Image
           </label>
+          <input
+            onChange={(e) => setName(e.target.value)}
+            value={name}
+            type="text"
+            required
+            placeholder="Your name"
+            className="p-2 border border-gray-500 rounded-md focus:outline-none focus:ring-2 focus:ring-violet-500"
+          />
+          <textarea
+            placeholder="Write profile bio"
+            required
+            className="p-2 border border-gray-500 rounded-md focus:outline-none focus:ring-2 focus:ring-violet-500"
+            rows={4}
+            onChange={(e) => setBio(e.target.value)}
+            value={bio}
+          ></textarea>
+          <button
+            type="submit"
+            className="bg-gradient-to-r from-purple-400 to-violet-600 text-white p-2 rounded-full text-lg cursor-pointer"
+          >
+            Save
+          </button>
         </form>
-        <img src="" alt="" className="" />
+        <img
+          src={assets.logo_icon}
+          alt=""
+          className="max-w-35 aspect-square rounded-full mx-10 max-sm:mt-10"
+        />
       </div>
     </div>
   );
