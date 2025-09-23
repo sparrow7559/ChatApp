6cabd73bf32fd542c532e24b05f191202317cf89
sparrow7559 <virajgupta2604@gmail.com>
2025-09-23 12:03:14 +0530

    Implement profile page with state management for user details and image upload

--- client/src/pages/ProfilePage.jsx
+++ client/src/pages/ProfilePage.jsx
@@ -1,9 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 const ProfilePage = () => {
+  const [selectedImage, setSelectedImage] = useState(null);
+  const navigate = useNavigate();
+  const [name, setName] = useState("Martin Johnson");
+  const [bio, setBio] = useState("Hi everyone, I am using QuickChat");
+
   return (
-    <div>
-      <h1>Profile Page</h1>
+    <div className="min-h-screen bg-cover bg-no-repeat flex items-center justify-center">
+      <div className="w-5/6 max-w-2xl backdrop-blur-2xl text-gray-300 border-2 border-gray-600 flex items-center justify-between max-sm:flex-col-reverse rounded-lg">
+        <form action="" className="flex flex-col gap-5 p-10 flex-1">
+          <h3 className="text-lg">Profile Details</h3>
+          <label
+            htmlFor="avatar"
+            className="flex items-center gap-3 cursor-pointer"
+          >
+            <input
+              type="file"
+              id="avatar"
+              accept=".png,.jpg,.jpeg"
+              className=""
+              hidden
+            />
+            <img src={""} alt="" className="" />
+          </label>
+        </form>
+        <img src="" alt="" className="" />
+      </div>
     </div>
   );
 };
