5a651798d8f24b2031b0a5b62f9973599aa80dec
sparrow7559 <virajgupta2604@gmail.com>
2025-09-26 02:04:35 +0530

    feat(controller): add signup function with validation for user details

---
+++ server/controllers/UserController.js
@@ -0,0 +1,12 @@
+import User from "../models/User";
+
+export const signup = async (req, res) => {
+  const { fullName, email, password, bio } = req.body;
+
+  try {
+    if (!fullName || !email || !password || !bio) {
+      return res.json({ success: false, message: "Missing Details" });
+    }
+    const user = await User.findOne({ email });
+  } catch (error) {}
+};
