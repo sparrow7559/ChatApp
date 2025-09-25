2fa001b032de6273f78d846297abc14e1f9da177
sparrow7559 <virajgupta2604@gmail.com>
2025-09-26 02:13:42 +0530

    feat(controller): enhance signup function with user existence check and password hashing

--- server/controllers/UserController.js
+++ server/controllers/UserController.js
@@ -1,4 +1,5 @@
 import User from "../models/User";
+import bcrypt from "bcryptjs";
 
 export const signup = async (req, res) => {
   const { fullName, email, password, bio } = req.body;
@@ -8,5 +9,12 @@ export const signup = async (req, res) => {
       return res.json({ success: false, message: "Missing Details" });
     }
     const user = await User.findOne({ email });
+
+    if (user) {
+      return res.json({ success: false, message: "Account already exists" });
+    }
+
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
   } catch (error) {}
 };
