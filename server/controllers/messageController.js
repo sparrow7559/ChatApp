59575430100875cbdb612244f80432f805b8a0f9
sparrow7559 <virajgupta2604@gmail.com>
2025-09-28 23:46:40 +0530

    Implement getUsersForSidebar function and fix receiverId casing in Message model

---
+++ server/controllers/messageController.js
@@ -0,0 +1,30 @@
+import Message from "../models/Message.js";
+import User from "../models/User.js";
+
+export const getUsersForSidebar = async (req, res) => {
+  try {
+    const userId = req.user._id;
+    const filteredUsers = await User.find({ _id: { $ne: userId } }).select(
+      "-password"
+    );
+
+    // count messages not seen
+    const unseenMessages = {};
+    const promises = filteredUsers.map(async (User) => {
+      const messages = await Message.find({
+        senderId: User._id,
+        receiverId: userId,
+        seen: false,
+      });
+      if (unseenMessages > 0) {
+        unseenMessages[User._id] = messages.length;
+      }
+    });
+
+    await Promise.all(promises);
+    res.json({ success: true, users: filteredUsers, unseenMessages });
+  } catch (error) {
+    console.log(error.message);
+    res.json({ success: false, messgae: error.message });
+  }
+};
--- server/models/Message.js
+++ server/models/Message.js
@@ -7,7 +7,7 @@ const messageSchema = new mongoose.Schema(
       ref: "User",
       required: true,
     },
-    ReceiverId: {
+    receiverId: {
       type: mongoose.Schema.Types.ObjectId,
       ref: "User",
       required: true,
