0f37eba57cb3a6d653cca6742487a47111de0366
sparrow7559 <virajgupta2604@gmail.com>
2025-09-29 21:31:52 +0530

    Add message routes for retrieving users and marking messages as seen

--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ import cors from "cors";
 import http from "http";
 import { connectDB } from "./lib/db.js";
 import userRouter from "./routes/userRoutes.js";
+import messageRouter from "./routes/messageRoutes.js";
 
 const app = express();
 const server = http.createServer(app);
@@ -13,6 +14,7 @@ app.use(cors());
 
 app.use("/api/status", (req, res) => res.send("Server is live"));
 app.use("/api/auth", userRouter);
+app.use("/api/messages", messageRouter);
 
 await connectDB();
 
