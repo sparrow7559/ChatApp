d3af7d53d319b25d5a59950cc74894454df6b8ef
sparrow7559 <virajgupta2604@gmail.com>
2025-09-29 19:32:54 +0530

    Add getMessages function to retrieve and mark messages as seen

--- server/controllers/messageController.js
+++ server/controllers/messageController.js
@@ -28,3 +28,26 @@ export const getUsersForSidebar = async (req, res) => {
     res.json({ success: false, messgae: error.message });
   }
 };
+
+export const getMessages = async (req, res) => {
+  try {
+    const { id: selectedUserId } = req.params;
+    const myId = req.user._id;
+
+    const messages = await Message.find({
+      $or: [
+        { senderId: myId, receiverId: selectedUserId },
+        { senderId: selectedUserId, receiverId: myId },
+      ],
+    });
+
+    await Message.updateMany(
+      { senderId: selectedUserId, receiverId: myId },
+      { seen: true }
+    );
+    res.json({ success: true, messages });
+  } catch (error) {
+    console.log(error.message);
+    res.json({ success: false, messgae: error.message });
+  }
+};
