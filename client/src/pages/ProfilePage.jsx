13f8cd4a0f8dd309a721398f63f570ac27d626e6
sparrow7559 <virajgupta2604@gmail.com>
2025-10-01 19:18:21 +0530

    Enhance ProfilePage to utilize AuthContext for user data; implement image upload and profile update functionality

--- client/src/pages/ProfilePage.jsx
+++ client/src/pages/ProfilePage.jsx
@@ -1,16 +1,32 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import assets from "../assets/assets";
+import { useContext } from "react";
+import { AuthContext } from "../../context/AuthContext";
 
 const ProfilePage = () => {
+  const { authUser, updateProfile } = useContext(AuthContext);
+
   const [selectedImage, setSelectedImage] = useState(null);
   const navigate = useNavigate();
-  const [name, setName] = useState("Martin Johnson");
-  const [bio, setBio] = useState("Hi everyone, I am using QuickChat");
+  const [name, setName] = useState(authUser.fullName);
+  const [bio, setBio] = useState(authUser.bio);
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    navigate("/");
+    if (!selectedImage) {
+      await updateProfile({ fullName: name, bio });
+      navigate("/");
+      return;
+    }
+
+    const reader = new FileReader();
+    reader.readAsDataURL(selectedImage);
+    reader.onload = async () => {
+      const base64Image = reader.result;
+      await updateProfile({ profilePic: base64Image, fullname: name, bio });
+      navigate("/");
+    };
   };
 
   return (
@@ -70,7 +86,9 @@ const ProfilePage = () => {
         <img
           src={assets.logo_icon}
           alt=""
-          className="max-w-35 aspect-square rounded-full mx-10 max-sm:mt-10"
+          className={`max-w-35 aspect-square rounded-full mx-10 max-sm:mt-10 ${
+            selectedImage && "rounded-full"
+          }`}
         />
       </div>
     </div>
