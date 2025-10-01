83bb427798b57345282f0dd1aa8ea01f5cf6df92
sparrow7559 <virajgupta2604@gmail.com>
2025-10-02 04:04:03 +0530

    Implement ChatContext with user management and message handling functionality

--- client/context/ChatContext.jsx
+++ client/context/ChatContext.jsx
@@ -1,8 +1,32 @@
-import { createContext } from "react";
+import { createContext, useContext, useState } from "react";
+import { AuthContext } from "./AuthContext";
+import toast from "react-hot-toast";
 
 export const ChatContext = createContext();
 
 export const ChatProvider = ({ children }) => {
+  const [messages, setMessages] = useState([]);
+  const [users, setUsers] = useState([]);
+
+  const [selectedUser, setSelectedUsers] = useState(null);
+  const [unseenMessages, setUnseenMessages] = useState({});
+
+  const { socket, axios } = useContext(AuthContext);
+
+  //function to get all users for sidebar
+
+  const getUsers = async () => {
+    try {
+      const { data } = await axios.get("/api/messages/users");
+      if (data.success) {
+        setUsers(data.users);
+        setUnseenMessages(data.unseenMessages);
+      }
+    } catch (error) {
+      toast.error(error.message);
+    }
+  };
+
   const value = {};
 
   return <ChatContext.Provider value={value}>{children}</ChatContext.Provider>;
