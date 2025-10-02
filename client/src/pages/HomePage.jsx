53e754e6656354b164d3717f6939bdce3cdef419
sparrow7559 <virajgupta2604@gmail.com>
2025-10-02 16:14:49 +0530

    Added selected user context from chat context

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -1,11 +1,11 @@
-import React, { useState } from "react";
+import React, { useContext, useState } from "react";
 import Sidebar from "../components/Sidebar";
 import ChatContainer from "../components/ChatContainer";
 import RightSidebar from "../components/RightSidebar";
+import { ChatContext } from "../../context/chatContext";
 
 const HomePage = () => {
-  const [selectedUser, setSelectedUser] = useState(false);
-
+  const { selectedUser } = useContext(ChatContext);
   return (
     <div className="border w-full h-screen sm:px-[15%] sm:py-[5%]">
       <div
@@ -17,10 +17,7 @@ const HomePage = () => {
       >
         <Sidebar />
         <ChatContainer />
-        <RightSidebar
-          selectedUser={selectedUser}
-          setSelectedUser={setSelectedUser}
-        />
+        <RightSidebar />
       </div>
     </div>
   );
